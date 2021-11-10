c0107e8478dd6e469c25404a8556c99d2bc3e4a6
shreyaspg <shreyasgaikwad197@gmail.com>
2021-11-10 09:29:55 +0530

    Not working

---
+++ build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+    echo "Usage: $0 <docker_image_and_tag>"
+    exit 1
+fi
+
+build_path=$(readlink -f "$(dirname "${BASH_SOURCE[0]}")")
+staging_dir="${build_path}/staging"
+# echo $staging_dir
+
+trap 'cleanup' EXIT
+cleanup() {
+  rm -rf "$staging_dir"
+}
+mkdir -p "$staging_dir"
+
+cp "${build_path}/client" "${staging_dir}/"
+cp "${build_path}/server" "${staging_dir}/"
+cp "${build_path}/crypto.py" "${staging_dir}/"
+cp "${build_path}/Dockerfile" "${staging_dir}/"
+cp "${build_path}/utils.py" "${staging_dir}/"
+
+pushd "$staging_dir"
+  docker build -t "shrys197/fx-private:encrypt_test" .
+#   docker push "shrys197/fx-private:encrypt_test"
+popd
---
+++ run.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+if [ -z "$1" ]; then
+    echo "Usage: $0 <server/client>"
+    exit 1
+fi
+
+
+volume="${PWD}/data/"
+echo $volume
+CONTAINER_NAME=federated-learning-demo
+
+# # start container
+# docker run --name $CONTAINER_NAME \
+#     --device /dev/isgx:/dev/isgx --device /dev/gsgx:/dev/gsgx -v /var/run/aesmd/aesm.socket:/var/run/aesmd/aesm.socket \
+#     -v "$PWD"/data/:/data --net=host \
+#     shrys197/fx-private:encrypt_test $1
+
+# start container   
+docker run \
+    -v $volume:/data --net=host --rm \
+    shrys197/fx-private:encrypt_test $1
+
+    # --device /dev/sgx:/dev/sgx -v /var/run/aesmd:/var/run/aesmd \
